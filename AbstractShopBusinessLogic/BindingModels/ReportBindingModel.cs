b6f0f963ea3cb0d97e5093e245e9e56046d44ed0
bertikq <bertikq@yandex.ru>
2020-06-02 18:10:28 +0400

    last fix

--- AbstractShopBusinessLogic/BindingModels/ReportBindingModel.cs
+++ AbstractShopBusinessLogic/BindingModels/ReportBindingModel.cs
@@ -7,7 +7,5 @@ namespace AbstractTravelCompanyBusinessLogic.BindingModels
     public class ReportBindingModel
     {
         public string FileName { get; set; }
-        public DateTime? DateFrom { get; set; }
-        public DateTime? DateTo { get; set; }
     }
 }
